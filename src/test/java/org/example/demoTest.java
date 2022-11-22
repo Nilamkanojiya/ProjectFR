e2e05e8708310083ac6de01c31f3bd1745aee2f3
nilam.kanojiya <nilam_kanojiya@yahoo.com>
2022-11-22 23:53:47 +0000

    contested 22/11

--- src/test/java/org/example/demoTest.java
+++ src/test/java/org/example/demoTest.java
@@ -2,13 +2,12 @@ package org.example;
 
 import com.microsoft.playwright.*;
 import com.microsoft.playwright.options.*;
-
 import java.nio.file.Paths;
 
 import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 
 public class demoTest {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         try (Playwright playwright = Playwright.create()) {
            Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions()
         .setHeadless(false).setSlowMo(3000));
@@ -78,9 +77,8 @@ public class demoTest {
             page.getByRole(AriaRole.COMBOBOX, new Page.GetByRoleOptions().setName("What stage has the divorce / dissolution case reached ?")).selectOption("1: Petition Issued");
             page.getByLabel("Name of Court / Divorce Centre where petition issued").click();
             page.getByLabel("Name of Court / Divorce Centre where petition issued").fill("watford court");
-            page.getByLabel("Upload Petition").click();
-            //TODO fix this
-            page.getByLabel("Upload Petition").setInputFiles(Paths.get("demo draft .rtf"));
+           // page.getByLabel("Upload Petition").click();
+            page.getByLabel("Upload Petition").setInputFiles(Paths.get("Files/scratch.txt"));
             page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Continue")).click();
             assertThat(page).hasURL("https://manage-case.aat.platform.hmcts.net/cases/case-create/DIVORCE/FinancialRemedyContested/FR_solicitorCreate/FR_solicitorCreate4");
 
@@ -105,6 +103,49 @@ public class demoTest {
             page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Continue")).click();
             assertThat(page).hasURL("https://manage-case.aat.platform.hmcts.net/cases/case-create/DIVORCE/FinancialRemedyContested/FR_solicitorCreate/FR_solicitorCreate7");
 
+             page.getByLabel("Maintenance Pending Suit").check();
+            page.getByLabel("Lump Sum Order").check();
+            page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Continue")).click();
+            assertThat(page).hasURL("https://manage-case.aat.platform.hmcts.net/cases/case-create/DIVORCE/FinancialRemedyContested/FR_solicitorCreate/FR_solicitorCreate11");
+
+            page.getByRole(AriaRole.RADIO, new Page.GetByRoleOptions().setName("No")).check();
+            page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Continue")).click();
+            assertThat(page).hasURL("https://manage-case.aat.platform.hmcts.net/cases/case-create/DIVORCE/FinancialRemedyContested/FR_solicitorCreate/FR_solicitorCreate12");
+            page.getByLabel("I Don't Know").check();
+            page.getByLabel("£1 - £5 million").check();
+            page.locator("#netValueOfHome").click();
+            page.locator("#netValueOfHome").fill("1000000");
+            page.getByLabel("Not applicable").check();
+            page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Continue")).click();
+            assertThat(page).hasURL("https://manage-case.aat.platform.hmcts.net/cases/case-create/DIVORCE/FinancialRemedyContested/FR_solicitorCreate/FR_solicitorCreate13");
+
+
+            page.getByRole(AriaRole.COMBOBOX, new Page.GetByRoleOptions().setName("Please choose the Region in which the Applicant resides")).selectOption("1: wales");
+            page.getByRole(AriaRole.COMBOBOX, new Page.GetByRoleOptions().setName("If the FRC is not on the pilot, your application cannot proceed online. Please make a paper application direct to the FRC.")).selectOption("1: northwales");
+            page.getByRole(AriaRole.COMBOBOX, new Page.GetByRoleOptions().setName("Where is the Applicant’s Local Court?")).selectOption("2: FR_northwalesList_4");
+            page.getByRole(AriaRole.RADIO, new Page.GetByRoleOptions().setName("No")).check();
+            page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Continue")).click();
+            assertThat(page).hasURL("https://manage-case.aat.platform.hmcts.net/cases/case-create/DIVORCE/FinancialRemedyContested/FR_solicitorCreate/FR_solicitorCreate15");
+            page.getByRole(AriaRole.RADIO, new Page.GetByRoleOptions().setName("Yes")).check();
+            page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Continue")).click();
+            assertThat(page).hasURL("https://manage-case.aat.platform.hmcts.net/cases/case-create/DIVORCE/FinancialRemedyContested/FR_solicitorCreate/FR_solicitorCreate21");
+            page.getByLabel("Mediator Registration Number (URN)").click();
+            page.getByLabel("Mediator Registration Number (URN)").fill("1122");
+            page.getByLabel("Family Mediation Service Name").click();
+            page.getByLabel("Family Mediation Service Name").fill("apex service");
+            page.getByLabel("Sole Trader Name").click();
+            page.getByLabel("Sole Trader Name").fill("Apex ltd");
+            page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Continue")).click();
+            assertThat(page).hasURL("https://manage-case.aat.platform.hmcts.net/cases/case-create/DIVORCE/FinancialRemedyContested/FR_solicitorCreate/FR_solicitorCreate23");
+            page.getByLabel("No").check();
+            page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Continue")).click();
+            assertThat(page).hasURL("https://manage-case.aat.platform.hmcts.net/cases/case-create/DIVORCE/FinancialRemedyContested/FR_solicitorCreate/FR_solicitorCreate24");
+            page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Continue")).click();
+            assertThat(page).hasURL("https://manage-case.aat.platform.hmcts.net/cases/case-create/DIVORCE/FinancialRemedyContested/FR_solicitorCreate/submit");
+            //TODO- write assertion
+            page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Submit")).click();
+            page.navigate("https://manage-case.aat.platform.hmcts.net/cases/case-details/1669160249001746");
+            page.navigate("https://manage-case.aat.platform.hmcts.net/cases/case-details/1669160249001746#History");
 
         }
     }
